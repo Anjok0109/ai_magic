5796c9e0e1c6ff70c8393b01564d6bd152a9c8a4
ai_magic <61124880+Anjok0109@users.noreply.github.com>
2023-09-26 18:39:02 -0500

    Update test.py

--- test.py
+++ test.py
@@ -18,20 +18,9 @@ poll_widgets()  # Start polling
 root.mainloop()
 
 
-    TButton: A standard push-button widget.
-    TCheckbutton: A checkbutton widget that can be in two states, checked and unchecked.
-    TCombobox: A combination of a text field and a drop-down list.
-    TEntry: A basic text-entry widget.
-    TFrame: A simple container widget.
-    TLabel: A widget to display text or images.
-    TLabelFrame: A container widget, like a frame but with an optional title.
-    TMenubutton: A button that can pop up a menu when pressed.
-    TPanedwindow: A container widget that contains one or more resizable panes.
-    TProgressbar: A widget to show the progress of an operation.
-    TRadiobutton: A widget that creates a radio button.
-    TScale: A slider widget.
-    TScrollbar: A widget that provides a scroll bar.
-    TSeparator: A separator widget, often used to separate groups of other widgets.
-    TSizegrip: A size grip widget, typically seen at the bottom right corner of resizable windows.
-    TNotebook: A notebook (tabbed) widget.
-    TTreeview: A multicolumn listbox or tree view widget.
+    style.map('TCombobox',
+              fieldbackground=[('readonly', 'white')],
+              selectbackground=[('readonly', 'blue')],
+              selectforeground=[('readonly', 'white')],
+              background=[('readonly', 'yellow')]
+              )
