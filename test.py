378573ba88a6588bd45102129e230034d171d748
ai_magic <61124880+Anjok0109@users.noreply.github.com>
2023-09-26 18:16:32 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk, filedialog
+
+def inspect_widgets(window):
+    for child in window.winfo_children():
+        print(type(child), child.winfo_class(), child)
+        inspect_widgets(child)
+
+def poll_widgets():
+    print("------")
+    inspect_widgets(root)
+    root.after(1000, poll_widgets)  # poll every 1000ms (1 second)
+
+root = tk.Tk()
+ttk.Button(root, text="Open File Dialog", command=filedialog.askopenfilename).pack()
+
+poll_widgets()  # Start polling
+root.mainloop()
